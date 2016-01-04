3e984c4e4807f279e3fee9185a5217c75da41a45
Pub4Game <pub4game@gmail.com>
2016-01-04 18:04:48 +0300

    oops

--- src/main/java/ToolsPro/commands/MuteCommand.java
+++ src/main/java/ToolsPro/commands/MuteCommand.java
@@ -28,14 +28,14 @@ public class MuteCommand extends ToolsProCommand {
             sender.sendMessage(this.getPermissionMessage());
         } else {
             if (args.length < 2) {
-                Message.CMD_MUTE_USAGE.print(sender,"prefix:&7[&aMute&7]",'c');
+                Message.CMD_MUTE_USAGE.print(sender,"prefix:&7[&aMute&7]", 'c');
                 //sender.sendMessage(TextFormat.colorize("&7[&aMute&7] &cИспользуйте /mute <ник> <время> <seconds|minutes|hours|days>"));
             } else {
                 Config mute = new Config(new File(this.plugin.getDataFolder(), "mute.yml"), Config.YAML);
                 Player p = this.plugin.getServer().getPlayer(args[0]);
                 if (!args[1].matches("^[1-9]+\\d*$")) {
                     //sender.sendMessage(TextFormat.colorize("&7[&aMute&7] &cПожалуйста, введите верное число!"));
-                    Message.NOT_NUMBER.print(sender,"prefix:&7[&aMute&7]",'c');
+                    Message.NOT_NUMBER.print(sender,"prefix:&7[&aMute&7]", 'c');
                 } else {
                     Double times = this.plugin.round(Integer.parseInt(args[1]), 2);
                     Double timings;
@@ -54,7 +54,7 @@ public class MuteCommand extends ToolsProCommand {
                         } else if (args[2].matches("(?i)days|day|d")) {
                             timings = times * 86400;
                         } else {
-                            Message.NOT_TIME.print(sender, "prefix:&7[&aMute&7]",'c');
+                            Message.NOT_TIME.print(sender, "prefix:&7[&aMute&7]", 'c');
                             //sender.sendMessage(TextFormat.colorize("&7[&aMute&7] &cУкажите верное значение времени!"));
                             return true;
                         }
@@ -62,7 +62,7 @@ public class MuteCommand extends ToolsProCommand {
                         timings = times * 60;
                     }
                     if (timings > (30 * 86400)) {
-                        Message.CMD_MUTE_MORE30DAY.print(sender,"prefix:[&aMute&7]", 'c');
+                        Message.CMD_MUTE_MORE30DAY.print(sender, "prefix:[&aMute&7]", 'c');
                         //sender.sendMessage(TextFormat.colorize("&7[&aMute&7] &cВы не можете замутить игрока больше чем на 30 дней!"));
                     } else {
                         mute.set(args[0].toLowerCase(), System.currentTimeMillis() + Math.round(timings * 1000d));
@@ -76,12 +76,12 @@ public class MuteCommand extends ToolsProCommand {
                                 minutes + Message.MINUTES.getText('c') +
                                 seconds + Message.SECONDS.getText('c');
                         if (p instanceof Player) {
-                            Message.CMD_MUTE_MUTED.print(p, "prefix:&7[&aMute&7]",'c',timemute);
+                            Message.CMD_MUTE_MUTED.print(p, "prefix:&7[&aMute&7]",'c', timemute);
                             //p.sendMessage(TextFormat.colorize("&7[&aMute&7] &cВы получили мут на " + timemute + " и теперь не можете писать в чат"));
                         }
-                        Message.CMD_MUTE_MUTEDBYU.print(sender,"prefix:&7[&aMute&7]",'c','b',args[0].toLowerCase(),timemute);
+                        Message.CMD_MUTE_MUTEDBYU.print(sender, "prefix:&7[&aMute&7]", 'c', 'b', args[0].toLowerCase(), timemute);
                         //sender.sendMessage(TextFormat.colorize("&7[&aMute&7] &b" + args[0].toLowerCase() + " &cбыл замучен на " + timemute));
-                        Message.CMD_MUTE_MUTEINFO.getText("prefix:&7[Mute]",sender.getName(),args[0].toLowerCase(),timemute);
+                        this.plugin.info(sender, Message.CMD_MUTE_MUTEINFO.getText("prefix:&7[Mute]", sender.getName(), args[0].toLowerCase(), timemute));
                         //this.plugin.info(sender, "&7[Mute] " + sender.getName() + " замутил игрока " + args[0].toLowerCase() + " на " + timemute + "!");
                     }
                 }
