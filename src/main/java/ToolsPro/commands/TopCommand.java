0ea7eae65969598f39f50049238acdd63d7aaf19
Pub4Game <pub4game@gmail.com>
2016-02-14 19:03:04 +0300

    Small fix /top command

--- src/main/java/ToolsPro/commands/TopCommand.java
+++ src/main/java/ToolsPro/commands/TopCommand.java
@@ -4,6 +4,7 @@ import ToolsPro.ToolsPro;
 import ToolsPro.util.Message;
 import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.math.Vector3;
 
 /**
  * Created by Pub4Game on 19.12.2015.
@@ -23,8 +24,8 @@ public class TopCommand extends Commands {
             Message.YOU_DONT_HAVE_PERMISSION.print(sender, 'c');
         } else {
             if (sender instanceof Player) {
-                Message.CMD_TOP_TP_MESSAGE.print(sender, 'a');
-                ((Player) sender).teleport(((Player) sender).add(0, ((Player) sender).getLevel().getHighestBlockAt(((Player) sender).getFloorX(), ((Player) sender).getFloorZ()) + 1));
+                Message.CMD_TOP_TP_MESSAGE.print(sender, "prefix:&7[&aTop&7]", 'a');
+                ((Player) sender).teleport(new Vector3(((Player) sender).getFloorX(), ((Player) sender).getLevel().getHighestBlockAt(((Player) sender).getFloorX(), ((Player) sender).getFloorZ()) + 1, ((Player) sender).getFloorZ()));
             } else {
                 return Message.NEED_PLAYER.print(sender, "prefix:&7[&aTop&7]", 'c');
             }
